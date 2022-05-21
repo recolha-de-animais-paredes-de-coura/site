80626f7ba28f04fe82c3e47b87f6ad65f9fef792
OscarFM <ofmz@autistici.org>
2022-05-21 17:08:59 +0000

    First commit: Testing

---
+++ testing/data/gatos.js
@@ -0,0 +1,68 @@
+export const listaDeGatos = [
+    {
+      gatoId: "01",
+      gatoNome: "",
+      gatoImagem: "../assets/img/20220116_171207.jpg",
+      gatoDesc: "Olá eu sou... "
+    },
+    {
+      gatoId: "01",
+      gatoNome: "",
+      gatoImagem: "../assets/img/20220116_171207.jpg",
+      gatoDesc: "Olá eu sou... "
+    },
+    {
+      gatoId: "01",
+      gatoNome: "",
+      gatoImagem: "../assets/img/20220116_171207.jpg",
+      gatoDesc: "Olá eu sou... "
+    },
+    {
+      gatoId: "01",
+      gatoNome: "",
+      gatoImagem: "../assets/img/20220116_171207.jpg",
+      gatoDesc: "Olá eu sou... "
+    },
+    {
+      gatoId: "01",
+      gatoNome: "",
+      gatoImagem: "../assets/img/20220116_171207.jpg",
+      gatoDesc: "Olá eu sou... "
+    },
+    {
+      gatoId: "01",
+      gatoNome: "",
+      gatoImagem: "../assets/img/20220116_171207.jpg",
+      gatoDesc: "Olá eu sou... "
+    },
+    {
+      gatoId: "01",
+      gatoNome: "",
+      gatoImagem: "../assets/img/20220116_171207.jpg",
+      gatoDesc: "Olá eu sou... "
+    },
+    {
+      gatoId: "01",
+      gatoNome: "",
+      gatoImagem: "../assets/img/20220116_171207.jpg",
+      gatoDesc: "Olá eu sou... "
+    },
+    {
+      gatoId: "01",
+      gatoNome: "",
+      gatoImagem: "../assets/img/20220116_171207.jpg",
+      gatoDesc: "Olá eu sou... "
+    },
+    {
+      gatoId: "01",
+      gatoNome: "",
+      gatoImagem: "../assets/img/20220116_171207.jpg",
+      gatoDesc: "Olá eu sou... "
+    },
+    {
+      gatoId: "01",
+      gatoNome: "",
+      gatoImagem: "../assets/img/20220116_171207.jpg",
+      gatoDesc: "Olá eu sou... "
+    }
+  ]
